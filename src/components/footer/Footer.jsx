92eb31f3e38d19314d5d34e766f66479526e8bd8
Shubham Singh <subhamsinghpg@gmail.com>
2023-07-12 09:40:22 +0530

    Simple footer added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,26 @@
 import './Footer.css'
 import WaveImg from '../../img/wave.png'
+import GithubImg from '../../img/github.png'
+import Linkedin from '../../img/linkedin.png'
 
 function Footer() {
   return (
     <div className='footer'>
-        <img src={WaveImg} alt="" />
-        <div className='f-content'></div>
+        
+        <div className='f-content'>
+          <div className="social-icons">
+             <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">
+               <img src={Linkedin} alt="LinkedIn" />
+             </a>
+             <a href="https://github.com/" target="_blank" rel="noopener noreferrer">
+               <img src={GithubImg} alt="GitHub" />
+             </a>
+          </div>
+
+          <div className="email">
+             <span>subhamsinghpg@gmail.com</span>
+          </div>
+        </div>
     </div>
   )
 }
